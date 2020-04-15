756b91c03cc10c6baf121957e75e5c29817f2d1e
konia-s <shin.19951110.ya@gmail.com>
2020-04-16 03:00:24 +0900

    POSTの追加

--- setting.sh
+++ setting.sh
@@ -15,18 +15,22 @@ curl -X DELETE $API_URL/$1/$2/labels/help%20wanted -u $3:$4
 curl -X DELETE $API_URL/$1/$2/labels/invalid -u $3:$4
 curl -X DELETE $API_URL/$1/$2/labels/question -u $3:$4
 curl -X DELETE $API_URL/$1/$2/labels/wontfix -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/documentation -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/good%20first%20issue -u $3:$4
 
 json_file=`cat label.json`
 json_length=`echo ${json_file} | jq length`
-
-echo $json_file
-echo $json_length
 
 for i in `seq 0 $(expr ${json_length} - 1)`
 do
     name=`echo ${json_file} | jq .[${i}].name`
     color=`echo ${json_file} | jq .[${i}].color`
     description=`echo ${json_file} | jq .[${i}].description`
+    json_data=`echo "{\"name\":$name,\"color\":$color,\"description\":$description}"`
     
-    echo "${name} ${color} ${description}"
+    curl -X POST $API_URL/$1/$2/labels \
+      -u $3:$4 \
+      -H "Accept: application/json" \
+      -H "Content-type: application/json" \
+      -d $json_data
 done
