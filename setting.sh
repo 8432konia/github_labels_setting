624dd348394e6ae12b386c2127bd630ec94b7426
konia-s <shin.19951110.ya@gmail.com>
2020-04-16 02:34:21 +0900

    defaultの削除

--- setting.sh
+++ setting.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+API_URL="https://api.github.com/repos"
+
+if [ $# -ne 4 ]; then
+  echo "[usage]" 1>&2
+  echo "setup_labels.sh {owner} {repo} {user} {token} " 1>&2
+  exit 1
+fi
+
+curl -X DELETE $API_URL/$1/$2/labels/bug -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/duplicate -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/enhancement -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/help%20wanted -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/invalid -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/question -u $3:$4
+curl -X DELETE $API_URL/$1/$2/labels/wontfix -u $3:$4
+
 json_file=`cat label.json`
 json_length=`echo ${json_file} | jq length`
 
